2ff2a8433c9fd447cfe276ade4222d1f2142374d
Luciano2078 <luciano.pereiraaristimunho@gmail.com>
2022-09-03 17:20:49 -0300

    update-tests

---
+++ src/entity/product.spec.ts
@@ -0,0 +1,46 @@
+import Product from "./product";
+
+
+describe("Product unit tests", () => {
+
+    // test ID
+
+    it("should throw error when id is empty", () => {
+        expect(() => {
+            const product = new Product("", "Product 1", 100);
+        }).toThrowError("Id is required");
+    });
+
+    // test NAME
+
+    it("should throw error when name is empty", () => {
+        expect(() => {
+            const product = new Product("123", "", 100);
+        }).toThrowError("Name is required");
+    });
+
+    // test PRICE
+
+    it("should throw error when price is less than zero", () => {
+        expect(() => {
+            const product = new Product("123", "Product 1", -1);
+        }).toThrowError("Price must be greater than zero");
+    });
+
+    // test CHANGE NAME
+
+    it("should change name", () => {
+        const product = new Product("122", "Product 1", 100);
+        product.changeName("Product 2");
+        expect(product.name).toBe("Product 2");
+    });
+
+    // test CHANGE PRICE
+
+    it("should change price", () => {
+        const product = new Product("123", "Product 1", 100);
+        product.changePrice(150);
+        expect(product.price).toBe(150);
+    });
+
+});
